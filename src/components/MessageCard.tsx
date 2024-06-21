f1e9af5b2dfe793ceae4155e1262f679cc45b125
Shubbu03 <thatcoderguyshubham@gmail.com>
2024-06-21 15:52:57 +0530

    refactor: Remove commented-out code in MessageCard component

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -46,37 +46,6 @@ const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
     });
     onMessageDelete(message._id as string);
   };
-  // return (
-  //   <Card>
-  //     <CardHeader>
-  //       <CardTitle>Card Title</CardTitle>
-  //       <AlertDialog>
-  //         <AlertDialogTrigger asChild>
-  //           <Button variant="destructive">
-  //             <X className="w-5 h-5" />
-  //           </Button>
-  //         </AlertDialogTrigger>
-  //         <AlertDialogContent>
-  //           <AlertDialogHeader>
-  //             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-  //             <AlertDialogDescription>
-  //               This action cannot be undone. This will permanently delete your
-  //               account and remove your data from our servers.
-  //             </AlertDialogDescription>
-  //           </AlertDialogHeader>
-  //           <AlertDialogFooter>
-  //             <AlertDialogCancel>Cancel</AlertDialogCancel>
-  //             <AlertDialogAction onClick={handleDeleteConfirm}>
-  //               Delete
-  //             </AlertDialogAction>
-  //           </AlertDialogFooter>
-  //         </AlertDialogContent>
-  //       </AlertDialog>
-  //       <CardDescription>Card Description</CardDescription>
-  //     </CardHeader>
-  //     <CardContent></CardContent>
-  //   </Card>
-  // );
 
   return (
     <Card className="card-bordered">
